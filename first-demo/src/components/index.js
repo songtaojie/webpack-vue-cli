e43b697391c5717f3faf23740bb80461ec32085c
songtaojie <stjworkemail@163.com>
2019-08-05 19:37:19 +0800

    添加组件

---
+++ first-demo/src/components/index.js
@@ -0,0 +1,2 @@
+import header from './Header.vue'
+export default{ header  }
---
+++ first-demo/src/views/Index.vue
@@ -0,0 +1,28 @@
+<style lang="scss" scoped>
+
+</style>
+<template>
+    <div>
+        <v-header title="首页">
+            <router-link slot="right" v-if="user.id" to="/home">{{user.name}}</router-link>
+        </v-header>
+        <div class="login-msg" v-if="!user.id">
+            <router-link to="/login">您还未登录,请先登录!</router-link>
+        </div>
+        <div class="" v-if="user.id">
+            <img width="50" :src="logo"><br>登陆成功!
+        </div>
+    </div>
+</template>
+<script>
+import mapState from 'vuex'
+import logo from '../assets/logo.png'
+export default {
+    data() {
+        return {
+            logo
+        }
+    },
+    computed: mapState({user:state.user}),
+}
+</script>
